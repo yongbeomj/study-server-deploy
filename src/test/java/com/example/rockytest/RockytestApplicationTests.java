360b2d34f27636cabebb4b67e6aa482f5f51e8e4
Yongbeom Jang <alex.ybjang@gmail.com>
2024-08-15 21:40:17 +0900

    Initial commit

---
+++ src/test/java/com/example/rockytest/RockytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rockytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
