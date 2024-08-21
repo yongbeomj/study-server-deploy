eef37cb450821fdbb47a0e37521fd659abc58622
Yongbeom Jang <alex.ybjang@gmail.com>
2024-08-21 21:45:53 +0900

    멀티모듈 프로젝트로 변경

--- src/main/java/com/example/rockytest/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.rockytest.repository;
-
-import com.example.rockytest.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
